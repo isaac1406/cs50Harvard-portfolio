6c81ea8eca49a19e280d91160f73761997a5af6e
isaac1406 <isaac.maiolino@gmail.com>
2024-02-08 21:46:38 +0000

    Thu, Feb 8, 2024, 6:46 PM -03:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,6 +1,8 @@
 #include <cs50.h>
 #include <stdio.h>
 
+void validation(int a);
+
 int main(void)
 {
     long number;
@@ -9,13 +11,14 @@ int main(void)
         number = get_long("Number: ");
     }
     while(number < 1);
+    validation(number);
 }
 
 void validation(int a)
 {
     while(a > 0)
     {
-        int b = a % 10
-        
+        int b = a % 10;
+        printf("%i ", b);
     }
 }
