4ae49892d99adc6b697185c63eb94f461a6d04a4
isaac1406 <isaac.maiolino@gmail.com>
2024-02-20 13:21:47 +0000

    Tue, Feb 20, 2024, 10:21 AM -03:00

--- credit/credit.c
+++ credit/credit.c
@@ -44,41 +44,9 @@ int main(void)
     }
 }
 
-int validation(long a)
+int every_(long a)
 {
-    int rest = 0, c = 0, multiply =  0, doubled = 0, total;
-    while(a > 0)
-    {
-        int b = a % 10;
-        rest += b;
-        a /= 10;
-        if(a > 0)
-        {
-            c = a % 10;
-            multiply = c * 2;
-            if(multiply > 10)
-            {
-                int fdigit = multiply / 10;
-                int ldigit = multiply % 10;
-                multiply = fdigit + ldigit;
-            }
-            doubled += multiply;
-            a /= 10;
-        }
-        total = doubled + rest;
-    }
-    return total;
-}
 
-int digits(long card)
-{
-    int counter = 0;
-    while(card > 0)
-    {
-        card /= 10;
-        counter++;
-    }
-    return counter;
 }
 
 bool IsAmex(long card, int numDigits)
