b0b9446313493e6e839840c81ea31a2f1bc15ed9
isaac1406 <isaac.maiolino@gmail.com>
2024-02-29 18:29:40 +0000

    Thu, Feb 29, 2024, 3:29 PM -03:00

---
+++ scrabble/scrabble.c
@@ -0,0 +1,34 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+void points(string player1);
+
+int main(void)
+{
+    string player1 = get_string("player 1: ");
+
+    points(player1);
+}
+
+void points(string player1)
+{
+    char one[10] = {'A', 'E', 'I', 'L', 'N', 'O', 'R', 'S', 'T', 'U'};
+
+    int sum = 0;
+
+    for(int i = 0; i < 2; i++)
+    {
+        for(int j = 0, lenght = strlen(player1); j < lenght; j++)
+        {
+            for(int h = 0; h < 11; h++)
+            {
+                if(player1[j] == one[h])
+                {
+                    sum += 1;
+                    return;
+                }
+            }
+        }
+    }
+}
