f79b519a315240b96df4fceb19ea0feec3be267c
isaac1406 <isaac.maiolino@gmail.com>
2024-02-29 18:58:38 +0000

    Thu, Feb 29, 2024, 3:58 PM -03:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -8,12 +8,12 @@ void points(string player1);
 int main(void)
 {
     string player1 = get_string("player 1: ");
-    for(int x = 0, lenght = strlen(player1); x < lenght; x++)
-    {
-        toupper(player1[x])
+   
+        string newone = toupper(player1[x]);
     }
+    printf("%s", newone);
 
-    points(player1);
+    points(newone);
 }
 
 void points(string player1)
