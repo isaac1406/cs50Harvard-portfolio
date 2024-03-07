4d350c00a26ac7c1bdbd9bdfb90fae6c80981bbb
isaac1406 <isaac.maiolino@gmail.com>
2024-03-07 19:44:49 +0000

    Thu, Mar 7, 2024, 4:44 PM -03:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -16,9 +16,11 @@ int main(int argc, string argv[])
     }
 
     // check if the command-line argument contains only digits
-    if (only_digits(argv[]) == false)
-    
-
+    if (only_digits(argv[1]) == false)
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
 }
 
 bool only_digits(string s)
