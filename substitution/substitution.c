5effe243ed5a581985a8c1f78f65156dad11336f
isaac1406 <isaac.maiolino@gmail.com>
2024-03-08 14:13:57 +0000

    Fri, Mar 8, 2024, 11:13 AM -03:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -19,6 +19,11 @@ int main(int argc, string argv[])
     // validate the key
     if (validation(argv[1]) == false)
     {
+        if (strlen(argv[1]) != 26)
+        {
+            printf("Key must contain 26 characters.");
+            return 1;
+        }
         printf("Usage: ./substitution key\n");
         return 1;
     }
@@ -37,7 +42,6 @@ bool validation(string s)
     // check if there are 26 letters
     if (strlen(s) != 26)
     {
-        
         return false;
     }
 
