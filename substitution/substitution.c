36b73e744a302fd795f5ad648828da8113f122ad
isaac1406 <isaac.maiolino@gmail.com>
2024-03-08 13:42:45 +0000

    Fri, Mar 8, 2024, 10:42 AM -03:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -3,3 +3,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+bool validation(string s);
+
+int main(int argc, string argv[])
+{
+
+}
+
+bool validation(string s)
+{
+    // check if there is only digits in the command-line argument
+    for (int i = 0, lenght = strlen(s); i < lenght; i++)
+    {
+        if (isdigit(s[i]) == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
