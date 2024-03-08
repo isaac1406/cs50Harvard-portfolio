2ac0e38f5b7fd204f7f875bea5ed293b281ec2d7
isaac1406 <isaac.maiolino@gmail.com>
2024-03-08 13:50:01 +0000

    Fri, Mar 8, 2024, 10:50 AM -03:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -13,15 +13,28 @@ int main(int argc, string argv[])
 
 bool validation(string s)
 {
-    // check if there is only digits in the command-line argument
+    // check if there are 26 letters
+    if (strlen(s) != 26)
+    {
+        return false;
+    }
+
     for (int i = 0, lenght = strlen(s); i < lenght; i++)
     {
+        // check if all chars are letters
         if (isalpha(s[i]) == 0)
         {
             return false;
         }
 
-        for (int j = 0; )
+        // check if there isn't any repeated letters
+        for (int j = 0; j < i; j++)
+        {
+            if (s[i] == s[j])
+            {
+                return false;
+            }
+        }
     }
     return true;
 }
