1516b31b223c9480736ba141871248f2cddef92b
isaac1406 <isaac.maiolino@gmail.com>
2024-03-18 15:16:08 +0000

    Mon, Mar 18, 2024, 12:16 PM -03:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -144,7 +144,10 @@ void tabulate(void)
     //
     for (int i = 0; i < voter_count; i++)
     {
-        
+        for (int j = 0; j < candidate_count; j++)
+        {
+
+        }
     }
     return;
 }
