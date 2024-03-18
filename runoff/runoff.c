3dc69ae1efbfd328f920d53617f4f2509ec7df77
isaac1406 <isaac.maiolino@gmail.com>
2024-03-18 18:04:26 +0000

    Mon, Mar 18, 2024, 3:04 PM -03:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -197,8 +197,18 @@ int find_min(void)
 bool is_tie(int min)
 {
     // TODO
-    
-    return false;
+    int remaining = 0, tied = 0;
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (!candidates[i].eliminated)
+            remaining++;
+        if (candidates[i].votes == min)
+            tied++;
+    }
+    if (tied == remaining)
+        return true;
+    else
+        return false;
 }
 
 // Eliminate the candidate (or candidates) in last place
