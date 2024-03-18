b52f2d324bc23849e41f71ed28a4c954d1c82bf1
isaac1406 <isaac.maiolino@gmail.com>
2024-03-18 18:30:33 +0000

    Mon, Mar 18, 2024, 3:30 PM -03:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -176,7 +176,7 @@ bool print_winner(void)
     {
         if (candidates[i].votes > (voter_count))
         {
-            printf("%s\n", candidates[i].name)
+            printf("%s\n", candidates[i].name);
             return true;
         }
     }
