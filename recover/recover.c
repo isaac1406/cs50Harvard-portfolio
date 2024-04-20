23a2fb71e037cb155d3c6894deedf81ddc2bde90
isaac1406 <isaac.maiolino@gmail.com>
2024-04-20 18:34:50 +0000

    Sat, Apr 20, 2024, 3:34 PM -03:00

--- recover/recover.c
+++ recover/recover.c
@@ -18,7 +18,9 @@ int main(int argc, char *argv[])
 
     // While there's still data left to read from the memory card
     while(fread(buffer, 1, 512, card) == 512){
-        if (buffer[0] == 0xff && )
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[3] == 0xff && (buffer[3] & 0xf0) == 0xe0){
+            
+        }
         char *name = malloc(8);
 
     }
