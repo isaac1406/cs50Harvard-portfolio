b73b75fb39c755d9c2f922b7c5d5541fda22447a
isaac1406 <isaac.maiolino@gmail.com>
2024-04-20 17:54:47 +0000

    Sat, Apr 20, 2024, 2:54 PM -03:00

--- recover/recover.c
+++ recover/recover.c
@@ -4,11 +4,13 @@
 
 int main(int argc, char *argv[])
 {
+    // Accept a single command-line argument
     if (argc != 2){
         printf("Usage: ./recover FILE\n");
         return 1;
     }
 
+    // Open the memory card
     FILE *card = fopen(argv[1], "r");
 
     uint8_t buffer[512];
