cace243c34f02bb296590da73a94bcec4d2f0592
isaac1406 <isaac.maiolino@gmail.com>
2024-04-20 18:53:19 +0000

    Sat, Apr 20, 2024, 3:53 PM -03:00

--- recover/recover.c
+++ recover/recover.c
@@ -17,11 +17,11 @@ int main(int argc, char *argv[])
     uint8_t buffer[512];
 
     int count = 0;
+    char *name = malloc(8);
 
     // While there's still data left to read from the memory card
     while(fread(buffer, 1, 512, card) == 512){
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[3] == 0xff && (buffer[3] & 0xf0) == 0xe0){
-            char *name = malloc(8);
             sprintf(name, "%03i.jpg", count);
             count++;
             FILE *img = fopen(name, "w");
@@ -32,7 +32,8 @@ int main(int argc, char *argv[])
             FILE *img = fopen(name, "w");
             fwrite(buffer, 1, 512, img);
         }
-
     }
+    free(name);
+    fclose
 
 }
