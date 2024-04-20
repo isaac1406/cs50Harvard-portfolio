ee10791ce1cd5c77575afb71af4740549e10d34a
isaac1406 <isaac.maiolino@gmail.com>
2024-04-20 17:48:43 +0000

    Sat, Apr 20, 2024, 2:48 PM -03:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,7 +1,14 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 int main(int argc, char *argv[])
 {
- 
+    if (argc != 2){
+        printf("Usage: ./recover FILE\n");
+        return 1;
+    }
+
+    FILE *card = fopen(argv[1], "r")
+
 }
