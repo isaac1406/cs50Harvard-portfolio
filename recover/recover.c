25a626a91893161da0e85c66e03eabadc6912f58
isaac1406 <isaac.maiolino@gmail.com>
2024-04-26 14:42:11 +0000

    Fri, Apr 26, 2024, 11:42 AM -03:00

--- recover/recover.c
+++ recover/recover.c
@@ -27,7 +27,7 @@ int main(int argc, char *argv[])
 
     // While there's still data left to read from the memory card
     while(fread(buffer, 1, 512, card) == 512){
-        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[3] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
             found = true;
         if (found == true){
             if (count != 0){
