e33994074ab1e96636b279f7aad7e8afa69a6fce
isaac1406 <isaac.maiolino@gmail.com>
2024-07-25 19:42:45 +0000

    Thu, Jul 25, 2024, 4:42 PM -03:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -53,14 +53,19 @@ person *create_family(int generations)
         new_person -> parents[1] = parent1;
 
         // Randomly assign current person's alleles based on the alleles of their parents
+        new_person -> alleles[0] = parent0 -> alleles[rand() % 2];
+        new_person -> alleles[1] = parent1 -> alleles[rand() % 2];
     }
 
     // If there are no generations left to create
     else
     {
-        // TODO: Set parent pointers to NULL
+        // Set parent pointers to NULL
+        new_person -> parents[0] = NULL;
+        new_person -> parents[0] = NULL;
 
         // TODO: Randomly assign alleles
+        new_person -> alleles = 
     }
 
     // TODO: Return newly created person
