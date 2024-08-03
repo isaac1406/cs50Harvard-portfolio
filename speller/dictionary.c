7dcbe3131a0dd4df37bd3f56833ad7ca3b5542f4
isaac1406 <isaac.maiolino@gmail.com>
2024-08-03 14:57:33 +0000

    Sat, Aug 3, 2024, 11:57 AM -03:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -36,7 +36,12 @@ unsigned int hash(const char *word)
 // Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary)
 {
-     // Open the dictionary file
+    // initialize hash table
+    for (int i = 0; i < N; i++)
+    {
+        table[i] = NULL;
+    }
+    // Open the dictionary file
     FILE *source = fopen(dictionary, "r");
     if (source == NULL)
     {
