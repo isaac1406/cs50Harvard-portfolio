003336e06a8eb3c10344d13474124240e8e9b470
isaac1406 <isaac.maiolino@gmail.com>
2024-08-08 23:05:38 +0000

    Thu, Aug 8, 2024, 8:05 PM -03:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,9 +1,20 @@
 from cs50 import get_float
 
 while True:
-    n = get_int("Change: ")
+    n = get_float("Change: ")
     if n > 0:
         break
 
+coins = 0
 while n > 0:
-    
+    if n >= 25:
+        n -= 25
+    elif n >= 10:
+        n -= 10
+    elif n >= 5:
+        n -= 5
+    else:
+        n -= 1
+    coins += 1
+
+print(coins)
