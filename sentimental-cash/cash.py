ce2cf195faa902cf848c487817305792a09405d7
isaac1406 <isaac.maiolino@gmail.com>
2024-08-08 23:17:54 +0000

    Thu, Aug 8, 2024, 8:17 PM -03:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -8,18 +8,18 @@ while True:
 cents = round(n * 100)
 
 coins = 0
-while n > 0:
-    if n >= 25:
-        n -= 25
+while cents > 0:
+    if cents >= 25:
+        cents -= 25
         coins += 1
-    elif n >= 10:
-        n -= 10
+    elif cents >= 10:
+        cents -= 10
         coins += 1
-    elif n >= 5:
-        n -= 5
+    elif cents >= 5:
+        cents -= 5
         coins += 1
     else:
-        n -= 1
+        cents -= 1
         coins += 1
 
 print(coins)
