a9a2dee43cf2a188343cb589d9c3654acb90cc22
isaac1406 <isaac.maiolino@gmail.com>
2024-08-10 20:23:04 +0000

    Sat, Aug 10, 2024, 5:23 PM -03:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,22 +1,22 @@
 from cs50 import get_string
-
+# get text from input
 text = get_string("Text: ")
-
+# count number of letters in text
 numlet = 0
 for c in text:
     if c.isalpha():
         numlet += 1
-
+# count number of words in text
 numwor = len(text.split())
-
+# count number of sentences in text
 numsen = text.count(".") + text.count("!") + text.count("?")
-
+# calculate average number of letters per 100 words
 L = (numlet * 100) / numwor
-
+# calculate average number of letters per 100 words
 S = (numsen * 100) / numwor
-
+# calculate grade level
 grade = round(0.0588 * L - 0.296 * S - 15.8)
-
+# print grade level
 if (grade < 1):
     print("Before Grade 1")
 elif (grade >= 16):
