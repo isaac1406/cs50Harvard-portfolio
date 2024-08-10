a89b234c648bc15de56088c1e652aac8cffc97a5
isaac1406 <isaac.maiolino@gmail.com>
2024-08-10 19:32:06 +0000

    Sat, Aug 10, 2024, 4:32 PM -03:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,13 +1,18 @@
 from cs50 import get_string
 
 numlet = 0
-numwor = 0
+global numwor
 numsen = 0
 
 def main():
     text = get_string("Text: ")
 
 def counter(text):
-    numlet = len
+    numlet = len(text)
+    numwor = 0
     for i in range(len(text)):
-        if (text[i] = " ")
+        if text[i] = " ":
+            numlet -= 1
+        if text[i - 1] != ' ' & text[i] == ' ' & text[i + 1] != ' ':
+            numwor += 1
+
