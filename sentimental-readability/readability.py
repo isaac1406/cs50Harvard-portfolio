06bf0af642de5397787c23402286b36701832d8b
isaac1406 <isaac.maiolino@gmail.com>
2024-08-10 20:11:36 +0000

    Sat, Aug 10, 2024, 5:11 PM -03:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -13,13 +13,13 @@ numsen = text.count(".") + text.count("!") + text.count("?")
 
 L = numlet / (numwor * 100)
 S = numsen / (numwor * 100)
-    index = round(0.0588 * L - 0.296 * S - 15.8)
+grade = index = round(0.0588 * L - 0.296 * S - 15.8)
 
 
 
-    if (grade <= 1):
-        print("Before Grade 1")
-    elif (grade >= 16):
-        print("Grade 16+")
+if (grade <= 1):
+    print("Before Grade 1")
+elif (grade >= 16):
+    print("Grade 16+")
     else:
         print(f"Grade {grade}")
