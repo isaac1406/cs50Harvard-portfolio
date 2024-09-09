955425e78b0fc3ed9c2be8e114dbd2d8e590f7cf
isaac1406 <isaac.maiolino@gmail.com>
2024-09-09 19:23:53 +0000

    Mon, Sep 9, 2024, 4:23 PM -03:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,7 +1,7 @@
 SELECT name FROM people
 JOIN stars ON people.id = stars.person_id
 JOIN movies ON stars.movie_id = movies.id
-WHERE movies.id =
+WHERE movies.id IN
 (
     SELECT movies.id FROM movies
     JOIN stars ON movies.id = stars.movie_id
