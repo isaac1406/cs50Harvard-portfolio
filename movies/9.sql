83e74167fdb7a22dddb0d5f94688a0d993a5c5bf
isaac1406 <isaac.maiolino@gmail.com>
2024-09-09 19:57:45 +0000

    Mon, Sep 9, 2024, 4:57 PM -03:00

--- movies/9.sql
+++ movies/9.sql
@@ -2,4 +2,4 @@ SELECT DISTINCT name FROM people
 JOIN stars ON stars.person_id = people.id
 JOIN movies ON movies.id = stars.movie_id
 WHERE movies.year = 2004
-ORDER BY people.birth ASC;
+ORDER BY people.birth;
