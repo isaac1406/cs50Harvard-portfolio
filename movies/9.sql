ac7a8ea51260fec000895228e69af53e2d53c512
isaac1406 <isaac.maiolino@gmail.com>
2024-09-09 18:09:56 +0000

    Mon, Sep 9, 2024, 3:09 PM -03:00

--- movies/9.sql
+++ movies/9.sql
@@ -1,11 +1,5 @@
 SELECT DISTINCT name FROM people
-WHERE id IN
-(
-    SELECT person_id FROM stars
-    WHERE movie_id = (
-
-        SELECT id FROM movies
-        WHERE year = 2004
-    )
-)
-ORDER BY birth;
+JOIN stars ON people.id = stars.person_id
+JOIN movies ON stars.movie_id = movies.id
+WHERE movies.year = 2004
+ORDER BY people.birth;
