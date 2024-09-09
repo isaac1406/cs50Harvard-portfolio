e2b040a3ca52ed0047a2c10eaa577eb5bbbc3d91
isaac1406 <isaac.maiolino@gmail.com>
2024-09-09 15:30:09 +0000

    Mon, Sep 9, 2024, 12:30 PM -03:00

--- movies/9.sql
+++ movies/9.sql
@@ -1,3 +1,10 @@
-SELECT name, birth FROM people WHERE id IN
-(SELECT person_id FROM stars WHERE movie_id =
-(SELECT id FROM movies WHERE year = 2004));
+SELECT name, birth FROM people
+WHERE id IN
+(
+    SELECT person_id FROM stars
+    WHERE movie_id = (
+
+        SELECT id FROM movies
+        WHERE year = 2004
+    )
+);
