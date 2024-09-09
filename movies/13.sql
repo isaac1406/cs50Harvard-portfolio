c091c9b29429a6377c3e3eb772d115c364f3a58a
isaac1406 <isaac.maiolino@gmail.com>
2024-09-09 19:17:51 +0000

    Mon, Sep 9, 2024, 4:17 PM -03:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,8 +1,11 @@
 SELECT name FROM people
 JOIN stars ON people.id = stars.person_id
-WHERE movie_id =
+JOIN movies ON stars.movie_id = movies.id
+WHERE stars.movie_id IN
 (
     SELECT id FROM movies
+    JOIN stars ON movies.id = stars.movie_id
+    JOIN people ON stars.person_id = people.id
     WHERE people.name = 'Kevin Bacon'
     AND people.birth = 1958
 );
