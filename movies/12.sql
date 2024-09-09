8ea6ea09f282b0c672137d65fd63728ae557f89c
isaac1406 <isaac.maiolino@gmail.com>
2024-09-09 19:45:41 +0000

    Mon, Sep 9, 2024, 4:45 PM -03:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,4 +1,6 @@
 SELECT title FROM movies
 JOIN stars ON movies.id = stars.movie_id
 JOIN people ON stars.person_id = people.id
-WHERE people.name IN ('Bradley Cooper', 'Jennifer Lawrence');
+WHERE people.name IN ('Bradley Cooper', 'Jennifer Lawrence')
+GROUP BY movie.title
+HAVING COUNT(DISTINCT person.name) = 2;
