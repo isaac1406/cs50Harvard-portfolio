9de7ba617bb67a3fff396740e9a57a4e57ebabcd
isaac1406 <isaac.maiolino@gmail.com>
2024-09-10 23:43:38 +0000

    Tue, Sep 10, 2024, 8:43 PM -03:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -99,23 +99,28 @@ WHERE passengers.flight_id =
     LIMIT 1
 )
 AND passengers.passport_number IN
-AND phone_calls.day = 28
-AND phone_calls.month = 7
-AND phone_calls.year = 2023
-AND phone_calls.duration < 60
-AND phone_calls.caller IN
 (
-    SELECT phone_number FROM people
-    WHERE license_plate IN
+    SELECT passport_number FROM people
+    JOIN phone_calls
+    ON people.phone_number = phone_calls.receiver
+    WHERE phone_calls.day = 28
+    AND phone_calls.month = 7
+    AND phone_calls.year = 2023
+    AND phone_calls.duration < 60
+    AND phone_calls.caller IN
     (
-        SELECT license_plate
-        FROM bakery_security_logs
-        WHERE day = 28
-        AND month = 7
-        AND year = 2023
-        AND hour = 10
-        AND minute >= 15
-        AND minute <= 25
+        SELECT phone_number FROM people
+        WHERE license_plate IN
+        (
+            SELECT license_plate
+            FROM bakery_security_logs
+            WHERE day = 28
+            AND month = 7
+            AND year = 2023
+            AND hour = 10
+            AND minute >= 15
+            AND minute <= 25
+        )
     )
 );
 
