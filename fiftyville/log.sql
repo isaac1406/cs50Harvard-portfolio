1f89dbf7686f588ae608eff3f3e3707f97f9ac5e
isaac1406 <isaac.maiolino@gmail.com>
2024-09-11 00:16:45 +0000

    Tue, Sep 10, 2024, 9:16 PM -03:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -115,6 +115,15 @@ AND passengers.passport_number IN
     )
 );
 
+SELECT person_id FROM bank_accounts
+JOIN atm_transactions
+ON bank_accounts.account_number = atm_transactions.account_number
+WHERE day = 28
+AND month = 7
+AND year = 2023
+AND transaction_type = 'withdraw'
+AND atm_location = 'Leggett Street';
+
 SELECT flight_id, passport_number FROM passengers
 JOIN flights ON id = flight_id
 WHERE day = 29
@@ -153,7 +162,7 @@ WHERE day = 28
 AND month = 7
 AND year = 2023
 AND transaction_type = 'withdraw'
-AND atm_location = 'Leggett Street'
+AND atm_location = 'Leggett Street';
 
 SELECT account_number, transaction_type, amount
 FROM atm_transactions
