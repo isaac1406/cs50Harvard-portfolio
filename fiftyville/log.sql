65e6b02a137c2409db1a9c304e19377e316d3712
isaac1406 <isaac.maiolino@gmail.com>
2024-09-11 01:31:54 +0000

    Tue, Sep 10, 2024, 10:31 PM -03:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -42,40 +42,7 @@ AND people.id IN
     AND transaction_type = 'withdraw'
     AND atm_location = 'Leggett Street'
 );
-
-SELECT receiver
-FROM phone_calls
-WHERE day = 28
-AND month = 7
-AND year = 2023
-AND duration < 60
-AND caller IN
-(
-    SELECT phone_number FROM people
-    WHERE license_plate IN
-    (
-        SELECT license_plate
-        FROM bakery_security_logs
-        WHERE day = 28
-        AND month = 7
-        AND year = 2023
-        AND hour = 10
-        AND minute >= 15
-        AND minute <= 25
-    )
-    AND people.id IN
-    (
-        SELECT person_id FROM bank_accounts
-        JOIN atm_transactions
-        ON bank_accounts.account_number = atm_transactions.account_number
-        WHERE day = 28
-        AND month = 7
-        AND year = 2023
-        AND transaction_type = 'withdraw'
-        AND atm_location = 'Leggett Street'
-    )
-);
-
+-- 
 SELECT name, passport_number FROM people
 JOIN phone_calls
 ON people.phone_number = phone_calls.caller
