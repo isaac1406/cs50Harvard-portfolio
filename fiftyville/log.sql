95f0da1174edaf964a80c78f62063ac1b9d61a64
isaac1406 <isaac.maiolino@gmail.com>
2024-09-11 01:22:21 +0000

    Tue, Sep 10, 2024, 10:22 PM -03:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -260,5 +260,34 @@ JOIN phone_calls ON phone_number = receiver
 WHERE caller =
 (
     SELECT phone_number FROM people
-    WHERE name = 'Bruce'
+    WHERE phone_calls.day = 28
+    AND phone_calls.month = 7
+    AND phone_calls.year = 2023
+    AND phone_calls.duration < 60
+    AND phone_calls.caller IN
+    (
+        SELECT phone_number FROM people
+        WHERE license_plate IN
+        (
+            SELECT license_plate
+            FROM bakery_security_logs
+            WHERE day = 28
+            AND month = 7
+            AND year = 2023
+            AND hour = 10
+            AND minute >= 15
+            AND minute <= 25
+        )
+        AND people.id IN
+        (
+            SELECT person_id FROM bank_accounts
+            JOIN atm_transactions
+            ON bank_accounts.account_number = atm_transactions.account_number
+            WHERE day = 28
+            AND month = 7
+            AND year = 2023
+            AND transaction_type = 'withdraw'
+            AND atm_location = 'Leggett Street'
+        )
+    )
 );
