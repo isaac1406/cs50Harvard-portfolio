45b97dba0e8dcf84c8192d6a3f8e2cbc47f057b7
isaac1406 <isaac.maiolino@gmail.com>
2024-09-11 00:14:41 +0000

    Tue, Sep 10, 2024, 9:14 PM -03:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -118,6 +118,11 @@ AND passengers.passport_number IN
 SELECT person_id FROM bank_accounts
 JOIN atm_transactions
 ON bank_accounts.account_number = atm_transactions.account_number
+WHERE day = 28
+AND month = 7
+AND year = 2023
+AND transaction_type = 'withdraw'
+AND atm_location = 'Leggett Street'
 
 SELECT account_number, transaction_type, amount
 FROM atm_transactions
