be983c50f24ae7c79b6f3dc12e532ad5b6ab11d9
isaac1406 <isaac.maiolino@gmail.com>
2024-09-11 00:22:14 +0000

    Tue, Sep 10, 2024, 9:22 PM -03:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -37,6 +37,17 @@ AND phone_number IN
     AND month = 7
     AND year = 2023
     AND duration < 60
+)
+AND people.id IN
+(
+    SELECT person_id FROM bank_accounts
+    JOIN atm_transactions
+    ON bank_accounts.account_number = atm_transactions.account_number
+    WHERE day = 28
+    AND month = 7
+    AND year = 2023
+    AND transaction_type = 'withdraw'
+    AND atm_location = 'Leggett Street'
 );
 
 SELECT receiver
