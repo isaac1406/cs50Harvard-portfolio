19fe80124719182aac5c4c33a162e8514100ccff
isaac1406 <isaac.maiolino@gmail.com>
2024-10-07 21:21:41 +0000

    Mon, Oct 7, 2024, 6:21 PM -03:00

--- prog2/p1/ex2.c
+++ prog2/p1/ex2.c
@@ -116,8 +116,14 @@ int ler_gravar_resultados(FILE *parq, FILE *parq2, char *time_desejado)
     while(fscanf(parq, "%s %d x %d %s",
     placar.time1.nome, placar.time1.gols, placar.time2.gols, placar.time2.nome))
     {
-        if(strcmp())
+        if(strcmp(placar.time1.nome, time_grava.nome) == 0 || strcmp(placar.time2.nome, time_grava.nome) == 0)
+        {
+            contabiliza_pontuacao(&placar, &time_grava);
+            break;
+        }
     }
+
+    grava_pontos_time(parq2, time_grava)
 }
 void mostra_pontuacao_time(FILE *parq)
 {
