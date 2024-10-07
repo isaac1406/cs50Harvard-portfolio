aa321f1fec8b72cdca4b9bf4b35fa4cffe9107a5
isaac1406 <isaac.maiolino@gmail.com>
2024-10-07 23:30:09 +0000

    Mon, Oct 7, 2024, 8:30 PM -03:00

--- prog2/p1/ex2.c
+++ prog2/p1/ex2.c
@@ -135,7 +135,8 @@ void mostra_pontuacao_time(FILE *parq)
     tPontos time_lido;
     fseek(parq, 0, SEEK_SET);
 
-    while (fread(&time_lido, sizeof(tPontos), 1, parq) == 1) {
+    while (!feof(parq)) {
+        fread(&time_lido, sizeof(tPontos), 1, parq);
         printf("time: %s - jogos: %d, saldo de gols: %d, pontos: %d\n",
         time_lido.nome, time_lido.jogos, time_lido.saldo, time_lido.pontos);
     }
