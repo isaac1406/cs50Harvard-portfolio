a557f1b50dae8f9bbd2f8b9f3bc63eaa3b0b4e07
isaac1406 <isaac.maiolino@gmail.com>
2024-11-19 22:36:50 +0000

    Tue, Nov 19, 2024, 7:36 PM -03:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -27,6 +27,32 @@ def index():
     if request.method == "POST":
 
         # TODO: Add the user's entry into the database
+        name = request.form.get("name")
+        if not name:
+            return redirect("/")
+
+        month = request.form.get("month")
+        if not month:
+            return redirect("/")
+        try:
+            month = int(month)
+        except ValueError:
+            return redirect("/")
+        if month < 1 or month > 12:
+            return redirect("/")
+
+        day = request.form.get("day")
+        if not day:
+            return redirect("/")
+        try:
+            day = int(day)
+        except ValueError:
+            return redirect("/")
+        if day < 1 or day > 31:
+            return redirect("/")
+
+        # Insert data into database
+        db.execute("INSERT INTO birthdays (name, month, day) VALUES(?, ?, ?)", name, month, day)
 
         return redirect("/")
 
