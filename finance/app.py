69c0a606b909cc577b72919a3a3cac9d2fbae7df
isaac1406 <isaac.maiolino@gmail.com>
2024-11-21 18:29:04 +0000

    Thu, Nov 21, 2024, 3:29 PM -03:00

--- finance/app.py
+++ finance/app.py
@@ -45,7 +45,10 @@ def buy():
     if request.method == 'POST':
         symbol = request.form.get("symbol").upper()
         shares = request.form.get("shares")
+        
         if not symbol:
+            return apology("must provide symbol")
+        elif not shares or not shares.isdigit() or int(shares) <= 0:
             return apology("must provide a positive integer number of shares")
 
 
