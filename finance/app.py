73838861ade9122566798396c696c506f6e82565
isaac1406 <isaac.maiolino@gmail.com>
2024-11-22 15:01:45 +0000

    Fri, Nov 22, 2024, 12:01 PM -03:00

--- finance/app.py
+++ finance/app.py
@@ -86,7 +86,7 @@ def buy():
         db.execute("UPDATE users SET cash = cash - ? WHERE id = ?", total_cost, session["user_id"])
 
         # add the purchase to the history table
-        db.execute("INSERT INTO transactions (user_id, symbol, shares, price) VALUES (:user_id, :symbol, :shares, :price)",
+        db.execute("INSERT INTO transactions (id, symbol, shares, price) VALUES (:user_id, :symbol, :shares, :price)",
                    user_id=session["user_id"], symbol=symbol, shares=shares, price=price)
 
         flash(f"Bought {shares} shares of {symbol} for {usd(total_cost)}!")
